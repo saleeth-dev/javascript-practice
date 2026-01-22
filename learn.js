e5bf098425809e78f83d69795f1226cce9550a66
S H Mahammed Saleeth <selith514@gmail.com>
2026-01-22 20:45:27 +0530

    Day 1 – Javascript practice

---
+++ learn.js
@@ -0,0 +1,19 @@
+let user = "Saleeth";
+let age = 20;
+
+const square = (n) => n * n;
+
+console.log(`User: ${user}`);
+console.log(`Age next year: ${age + 1}`);
+console.log(`Square of 4 is ${square(4)}`);
+
+function add(a, b) {
+    return a + b;
+}
+
+n = 5
+m = n * n;
+console.log(`square of 5 is ${m}`);
+
+const double = (n) => n * n;
+console.log(`Square of 4 is ${double(4)}`);
