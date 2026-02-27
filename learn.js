86996de8e1b76cd9df6ae2d4fed4e868e530fbc6
S H Mahammed Saleeth <selith514@gmail.com>
2026-02-27 23:02:22 +0530

    Implement basic HTTP server in learn.js
    Added a simple HTTP server with routes for home, about, and API responses.

--- learn.js
+++ learn.js
@@ -16,4 +16,41 @@ m = n * n;
 console.log(`square of 5 is ${m}`);
 
 const double = (n) => n * n;
+
 console.log(`Square of 4 is ${double(4)}`);
+
+// this is practiece section
+
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+
+  if (req.url === "/") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("Welcome to My Node Server 🚀");
+  }
+
+  else if (req.url === "/about") {
+    res.writeHead(200, { "Content-Type": "text/plain" });
+    res.end("This is About Page");
+  }
+
+  else if (req.url === "/api") {
+    res.writeHead(200, { "Content-Type": "application/json" });
+    res.end(JSON.stringify({
+      name: "Saleeth",
+      role: "Full Stack Student",
+      status: "Learning Node.js"
+    }));
+  }
+
+  else {
+    res.writeHead(404, { "Content-Type": "text/plain" });
+    res.end("Page Not Found ❌");
+  }
+
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://localhost:3000");
+});
