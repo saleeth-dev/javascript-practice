79ade3f4b94ebb402c0fa76cf3e243235a63635a
S H Mahammed Saleeth <selith514@gmail.com>
2026-06-13 11:08:25 +0530

    Refactor learn.js with additional questions and examples

--- learn.js
+++ learn.js
@@ -27,7 +27,7 @@ const server = http.createServer((req, res) => {
 
   if (req.url === "/") {
     res.writeHead(200, { "Content-Type": "text/plain" });
-    res.end("Welcome to My Node Server 🚀");
+    res.end("Welcome to My Node Server ");
   }
 
   else if (req.url === "/about") {
@@ -46,7 +46,7 @@ const server = http.createServer((req, res) => {
 
   else {
     res.writeHead(404, { "Content-Type": "text/plain" });
-    res.end("Page Not Found ❌");
+    res.end("Page Not Found ");
   }
 
 });
@@ -54,3 +54,95 @@ const server = http.createServer((req, res) => {
 server.listen(3000, () => {
   console.log("Server running at http://localhost:3000");
 });
+
+// Question-1
+
+const num = [2, 4, 6, 8];
+const square = num.map(n => n * n);
+console.log(square);
+
+// Question-2
+
+const num2 = [1,2,3,4,5,6,7,8];
+const even = num2.filter(n => n%2 === 0);
+console.log(even);
+
+// Question-3   
+
+const total = [10,20,30,40].reduce((acc, curr) => acc + curr, 0);
+console.log(total);
+
+// Question-4
+const calculate = (operation, a, b) => {
+    return operation(a, b);
+};
+console.log(calculate((x, y) => x + y, 5, 3)); // Output: 8
+
+// Question-5
+function factorial(n) {
+    if (n === 0|| n === 1) {
+        return 1;
+    }
+
+    return n * factorial(n - 1);
+}
+console.log(factorial(5)); // Output: 120
+
+// Question-6
+class student {
+    constructor(name, course) {
+        this.name = Saleeth;
+        this.course = ai&ml;
+    }
+
+    getDetails() {
+        return `Name: ${this.name}, Course: ${this.course}`;
+    }
+}
+
+// Question-7
+const user={
+ name:"Saleeth",
+ age:20
+}
+const {name, age} = user;
+console.log(name);
+console.log(age);
+
+// Question-8
+const frontend=["HTML","CSS"];
+const backend=["Node","Express"];
+
+const fullstack=[...frontend,...backend];
+console.log(fullstack);
+
+// Question-9
+function multiply(a,b){
+    return(a * b);
+}
+console.log(multiply(5,3)); // Output: 15
+
+// Question-10
+const products=[
+ {name:"Laptop",price:50000},
+ {name:"Mouse",price:500},
+ {name:"Phone",price:30000}
+];
+
+const above1000 = products.filter(product => product.price > 1000);
+console.log(above1000);
+
+const productNames = products.map(product => product.name);
+console.log(productNames);
+
+const totalPrice = products.reduce((acc, product) => acc + product.price, 0);   
+console.log(totalPrice);
+
+// Question-11
+
+const nums = [5, 10, 3, 25, 8];
+
+const max = nums.reduce((acc, curr) => {
+    return curr > acc ? curr : acc;
+});
+console.log(max);
